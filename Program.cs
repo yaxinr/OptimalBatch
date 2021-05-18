721c4cf2d91dc1644cdf90999bb23ca6e5e20186
gilfanovrrPaker <gilfanovrr@npf-paker.ru>
2021-05-18 15:34:19 +0500

    batch reqs as list of tuples

--- Program.cs
+++ Program.cs
@@ -12,13 +12,13 @@ namespace OptimalBatch
         {
             Requirement[] reqs = SeedSmallLastBatch();
             Console.WriteLine("reqs.Sum={0}", reqs.Sum(r => r.quantity));
-            var optimalBatches = OptimalBatchStatic.GetOptimalBatches(reqs, 75, 30, 5);
+            var optimalBatches = OptimalBatchStatic.GetOptimalBatches(reqs, 122, 30, 29);
             foreach (var batch in optimalBatches)
             {
                 Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", batch.limit, batch.reserved, batch.Balance, batch.Quantity, batch.deadline);
                 foreach (var kv in batch.reqs)
                 {
-                    Console.WriteLine("\t{0}\t{1}", kv.Value, kv.Key.deadline);
+                    Console.WriteLine("\t{0}\t{1}", kv.Item2, kv.Item1.deadline);
                 }
             }
             Console.WriteLine("optimalBatches.Sum(b => b.Quantity)={0}", optimalBatches.Sum(b => b.Quantity));
@@ -28,9 +28,10 @@ namespace OptimalBatch
         private static Requirement[] SeedSmallLastBatch()
         {
             Requirement[] reqs = new Requirement[] {
-            new Requirement(74, new DateTime(2021, 2, 22) ),
-            new Requirement(76, new DateTime(2021, 2, 22) ),
-            new Requirement(10, new DateTime(2021, 3, 22) ),
+            new Requirement(10, new DateTime(2021, 5, 28) ),
+            new Requirement(12, new DateTime(2021, 5, 28) ),
+            new Requirement(12, new DateTime(2021, 5, 28) ),
+            new Requirement(48, new DateTime(2021, 6,14) ),
             };
             //var optimalBatches = OptimalBatchStatic.GetOptimalBatches(reqs, 75, 30, 5);
             return reqs;
@@ -66,7 +67,7 @@ namespace OptimalBatch
                             uint reserveQnt = Math.Min(batch.Balance, reqQnt);
                             batch.reserved += reserveQnt;
                             reqQnt -= reserveQnt;
-                            batch.reqs.Add(req, reserveQnt);
+                            batch.reqs.Add(Tuple.Create(req, reserveQnt));
 
                             if (reqQnt > 0)
                             {
@@ -87,7 +88,7 @@ namespace OptimalBatch
                         uint reserveQnt = Math.Min(batch.Quantity - batch.reserved, reqQnt);
                         batch.reserved += reserveQnt;
                         reqQnt -= reserveQnt;
-                        batch.reqs.Add(req, reserveQnt);
+                        batch.reqs.Add(Tuple.Create(req, reserveQnt));
                         if (reqQnt == 0) break;
                     }
                     else
@@ -119,7 +120,7 @@ namespace OptimalBatch
         public DateTime deadline;
         public uint limit;
         public object requirement;
-        public Dictionary<Requirement, uint> reqs;
+        public List<Tuple<Requirement, uint>> reqs;
         public uint reserved = 0;
         public uint frequency;
 
@@ -129,7 +130,7 @@ namespace OptimalBatch
             this.deadline = requirement.deadline;
             this.requirement = requirement.req;
             this.frequency = frequency;
-            this.reqs = new Dictionary<Requirement, uint>();
+            this.reqs = new List<Tuple<Requirement, uint>>();
         }
 
         public uint Balance => limit - reserved;
