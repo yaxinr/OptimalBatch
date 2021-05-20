f787de7420930ba21153d9e7c611be4647395f97
gilfanovrrPaker <gilfanovrr@npf-paker.ru>
2021-05-20 13:23:11 +0500

    batch size from min to max

--- Program.cs
+++ Program.cs
@@ -12,10 +12,11 @@ namespace OptimalBatch
         {
             Requirement[] reqs = SeedSmallLastBatch();
             Console.WriteLine("reqs.Sum={0}", reqs.Sum(r => r.quantity));
-            var optimalBatches = OptimalBatchStatic.GetOptimalBatches(reqs, 122, 30, 29);
+            var optimalBatches = OptimalBatchStatic.GetOptimalBatches(reqs, 26, 30, 1);
             foreach (var batch in optimalBatches)
             {
-                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", batch.limit, batch.reserved, batch.Balance, batch.Quantity, batch.deadline);
+                Console.WriteLine("limit={0}\treserv={1}\tbalance={2}\tqnt={3}\tdeadline={4}",
+                    batch.limit, batch.reserved, batch.Balance, batch.Quantity, batch.deadline);
                 foreach (var kv in batch.reqs)
                 {
                     Console.WriteLine("\t{0}\t{1}", kv.Item2, kv.Item1.deadline);
@@ -28,10 +29,22 @@ namespace OptimalBatch
         private static Requirement[] SeedSmallLastBatch()
         {
             Requirement[] reqs = new Requirement[] {
-            new Requirement(10, new DateTime(2021, 5, 28) ),
-            new Requirement(12, new DateTime(2021, 5, 28) ),
-            new Requirement(12, new DateTime(2021, 5, 28) ),
-            new Requirement(48, new DateTime(2021, 6,14) ),
+
+            //new Requirement(15, new DateTime(2021, 5, 26) ),
+            //new Requirement(1, new DateTime(2021, 6, 21) ),
+
+            new Requirement(8, new DateTime(2021, 7, 30) ),
+            new Requirement(18, new DateTime(2021, 8, 20) ),
+            new Requirement(2, new DateTime(2021, 8, 20) ),
+            new Requirement(20, new DateTime(2021, 8, 28) ),
+            //new Requirement(12, new DateTime(2021, 6,14) ),
+            //new Requirement(12, new DateTime(2021, 6,14) ),
+            //new Requirement(12, new DateTime(2021, 6,15) ),
+            //new Requirement(12, new DateTime(2021, 6,18) ),
+            //new Requirement(12, new DateTime(2021, 6,18) ),
+            //new Requirement(12, new DateTime(2021, 6,22) ),
+            //new Requirement(12, new DateTime(2021, 6,22) ),
+            //new Requirement(12, new DateTime(2021, 6,22) ),
             };
             //var optimalBatches = OptimalBatchStatic.GetOptimalBatches(reqs, 75, 30, 5);
             return reqs;
@@ -44,7 +57,11 @@ namespace OptimalBatch
         {
             var firstReq = requirements.First();
             List<Batch> batches = new List<Batch>();
-            var batch = new Batch(maxQuantity, firstReq, frequency);
+            uint firstBatchQnt = (uint)requirements.Where(r => r.deadline < firstReq.deadline.AddDays(days))
+                .Sum(r => r.quantity);
+            if (firstBatchQnt > maxQuantity * 2)
+                firstBatchQnt = maxQuantity;
+            var batch = new Batch(firstBatchQnt, firstReq, frequency);
             batches.Add(batch);
             for (int i = 0; i < requirements.Length; i++)
             {
@@ -53,7 +70,7 @@ namespace OptimalBatch
                 {
                     uint batchQnt = (uint)requirements.Skip(i).Where(r => r.deadline < req.deadline.AddDays(days))
                         .Sum(r => r.quantity);
-                    if (batchQnt > maxQuantity * 1.5)
+                    if (batchQnt > maxQuantity * 2)
                         batchQnt = maxQuantity;
                     batch = new Batch(batchQnt, req, frequency);
                     batches.Add(batch);
@@ -74,7 +91,7 @@ namespace OptimalBatch
                                 uint batchQnt = reqQnt +
                                     (uint)requirements.Skip(i + 1).Where(r => r.deadline < req.deadline.AddDays(days))
                                     .Sum(r => r.quantity);
-                                if (batchQnt > maxQuantity * 1.5)
+                                if (batchQnt > maxQuantity * 2)
                                     batchQnt = maxQuantity;
                                 batch = new Batch(batchQnt, req, frequency);
                                 batches.Add(batch);
